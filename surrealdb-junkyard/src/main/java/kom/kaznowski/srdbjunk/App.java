105b72fa804f760ec88ff1bd483a9ba94a6e7ea1
Hugh Kaznowski <hugh@surrealdb.com>
2023-04-21 14:41:45 +0100

    Generate project

---
+++ surrealdb-junkyard/src/main/java/kom/kaznowski/srdbjunk/App.java
@@ -0,0 +1,13 @@
+package kom.kaznowski.srdbjunk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
