7a8482f90c5884142cefbc29208ccf3cddddc0de
Hugh Kaznowski <hugh@surrealdb.com>
2023-05-02 15:20:32 +0100

    Some working code

--- src/main/java/kom/kaznowski/srdbjunk/App.java
+++ src/main/java/kom/kaznowski/srdbjunk/App.java
@@ -6,11 +6,7 @@ import com.surrealdb.driver.SyncSurrealDriver;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
@@ -20,10 +16,13 @@ public class App
         driver.signIn("root", "root");
         driver.use("namespace-name", "database-name");
         String tableName = "user";
+        driver.delete(tableName);
         User tobie = driver.create(tableName, User.builder().name("Tobie").build());
         User jaime = driver.create(tableName, User.builder().name("Tobie").build());
-        List<Map<String, String>> updates = driver.update(tableName, Map.of("name", "Jaime"));
+        List<Map<String, String>> updates = driver.update(jaime.id, Map.of(
+                "name", "Jaime"));
         List<User> allUsers = driver.select(tableName, User.class);
         System.out.printf("All users = %s", allUsers);
+        conn.disconnect();
     }
 }
